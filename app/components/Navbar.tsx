35cbdb372b2d2c7e24c42c6c8e8762a6022384a4
Brooke Kennison <brookelkennison@gmail.com>
2024-09-24 07:40:59 -0400

    complete navbar

---
+++ app/components/Navbar.tsx
@@ -0,0 +1,68 @@
+import React from 'react';
+import MyImage from '../public/assets/logo-dark.png';
+import Image from 'next/image';
+
+const Navbar: React.FC = () => {
+	return (
+		<header id='navbar' className='sticky top-0 z-50 duration-300'>
+			<nav className='relative container flex justify-between items-center'>
+				<div className='py-5 pe-4'>
+					<Image src={MyImage} height={50} width={50} alt='my logo' />
+				</div>
+				<div className='hidden absolute top-0 left-0 bg-primaryColor w-full py-24 lg:bg-transparent lg:block lg:static lg:py-0 lg:border-none lg:w-auto lg:ml-auto' id='nav-menu'>
+					<ul className='flex flex-col items-center text-center gap-8 lg:gap-5 lg:flex-row'>
+						<li>
+							<a href='#quality_services' className='nav-link'>
+								Services
+							</a>
+						</li>
+						<li>
+							<a href='#recent_works' className='nav-link'>
+								Works
+							</a>
+						</li>
+						<li>
+							<a href='#exp_edu' className='nav-link'>
+								Resume
+							</a>
+						</li>
+						<li>
+							<a href='#skills' className='nav-link'>
+								Skills
+							</a>
+						</li>
+						<li>
+							<a href='#blogs' className='nav-link'>
+								Blogs
+							</a>
+						</li>
+						<li>
+							<a href='#contact' className='nav-link'>
+								Contact
+							</a>
+						</li>
+						<li>
+							<button className='btn ml-10'>
+								<span className='z-10'>Hire Me!</span>
+							</button>
+						</li>
+					</ul>
+
+					<div className='absolute top-6 right-3 md:right-8 text-whiteColor text-3xl cursor-pointer lg:hidden' id='nav-close'>
+						<i className='ri-close-line'></i>
+					</div>
+				</div>
+
+				<div className='flex items-center gap-5'>
+					<i className='ri-moon-line cursor-pointer ml-4 text-2xl' id='theme-toggle'></i>
+
+					<div className='lg:hidden' id='hamburger'>
+						<i className='ri-menu-2-line cursor-pointer text-2xl'></i>
+					</div>
+				</div>
+			</nav>
+		</header>
+	);
+};
+
+export default Navbar;
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,9 @@
+import Navbar from './components/Navbar';
+
 export default function Home() {
 	return (
-		<div className='flex items-center justify-center h-screen bg-gray-100'>
-			<h1 className='text-4xl font-bold'>Welcome to my Portfolio!</h1>
+		<div className='flex items-center justify-center bg-gray-100'>
+			<Navbar />
 		</div>
 	);
 }
